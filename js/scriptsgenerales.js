efc7ae82ddbd12d28e094d8438ffc18496d71578
Franko Vidal <francoborottov@gmail.com>
2023-09-02 10:31:37 -0400

    Animaciones de login, generacion de qr funcional.

--- js/scriptsgenerales.js
+++ js/scriptsgenerales.js
@@ -25,3 +25,43 @@ function Terminarclase() {
         });
     }
 }
+
+function logout() { 
+    document.getElementById('cerrarSesion').addEventListener('click', function (event) {
+        // Evita que el enlace redireccione inmediatamente
+        event.preventDefault();
+
+        // Desactiva el botón para evitar clics repetidos
+        document.getElementById('cerrarSesion').classList.add('disabled');
+        
+        // Oculta el texto del botón y muestra la animación de carga
+        document.getElementById('botonTexto').style.display = 'none';
+        document.getElementById('animacionCarga').style.display = 'inline-block';
+
+        // Simula una pausa de 2 segundos (2000 milisegundos)
+        setTimeout(function () {
+            // Redirige al usuario a la página de inicio de sesión
+            window.location.href = "login.html";
+        }, 2000);
+    });
+}
+function mostrarConfirmacion() {
+    Swal.fire({
+        title: '¿Estás seguro de iniciar la clase?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, iniciar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Redirige a la página de asistencia.html
+            window.location.href = 'asistencia.html';
+        }
+    });
+}
+
+
+
+
+
+
